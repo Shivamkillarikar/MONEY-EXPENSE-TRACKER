c339c7e75a1eff61664cf1500cf30a575751a72f
Shivamkillarikar <143188904+Shivamkillarikar@users.noreply.github.com>
2024-11-25 13:44:18 +0530

    Update budgetRoutes.js

--- API/Routes/budgetRoutes.js
+++ API/Routes/budgetRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createOrUpdateBudget, getBudget } = require('../controllers/budgetController');
+const { createOrUpdateBudget, getBudget } = require('../Controllers/budgetController');
 
 const router = express.Router();
 
